e2be0aafeeab2c41763ef20f9178fc45049a5050
Daniel Ching <carterbox@users.noreply.github.com>
2019-06-10 15:43:33 -0500

    NEW: Add a script template for student final projects

---
+++ reconstruct.py
@@ -0,0 +1,20 @@
+"""Capstone Project: Write your own script!
+
+In this file, write your own reconstruction script to do the following:
+
+1. Load a dataset using dxchange.
+
+2. Apply one preprocessing algorithm to remove distortions.
+
+3. Reconstruct the data using one direct method.
+
+4. Reconstruct the data using one iterative algorithm. Use the direct
+reconstruction as the initial guess for the iterative algorithm.
+
+5. Save your reconstructed data using dxchange.
+"""
+
+import tomopy
+
+if __name__ == '__main__':
+    # Write your script here!
